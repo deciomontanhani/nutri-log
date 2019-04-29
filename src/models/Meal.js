43665a45d9b449fd21479d4ccee35551bcf3b3af
deciomontanhani <deciomontanhani@gmail.com>
2019-04-28 23:05:15 -0300

    dando início a lista de refeições

---
+++ src/models/Meal.js
@@ -0,0 +1,8 @@
+export default class Meal {
+  constructor(id, time, title, feeling) {
+    this.id = id;
+    this.time = time;
+    this.title = title;
+    this.feeling = feeling;
+  }
+}
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
 import {
-  Text, StyleSheet, View, StatusBar,
+  Text, StyleSheet, View, StatusBar, FlatList,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Meal from '~/models/Meal';
 
 const styles = StyleSheet.create({
   container: {
@@ -26,20 +27,59 @@ const styles = StyleSheet.create({
   },
 });
 
-const Main = () => (
-  <View style={styles.container}>
-    <View>
-      <StatusBar backgroundColor="#D6E7E0" />
-      <Text style={styles.dateLabel}>Segunda-feira, 23 de Abril de 2019</Text>
-    </View>
-    <View style={styles.content}>
-      <Text style={styles.contentTitle}>Hoje você comeu:</Text>
-    </View>
-  </View>
-);
+class Main extends React.Component {
+  state = {
+    meals: [
+      new Meal(1, '08:25h', 'Lanche Integral', 'Feliz e saciado'),
+      new Meal(2, '12:30h', 'Almoço Executivo', 'Ansioso e levemente saciado'),
+      new Meal(3, '14:00h', 'Frutas com Iogurte', 'Contente, com fome'),
+    ],
+  };
 
-Main.navigationOptions = {
-  title: 'Nutri Log',
-};
+  navigationOptions = {
+    title: 'Nutri Log',
+  };
+
+  keyExtractor = (item, index) => String(item.id);
+
+  renderItem = ({ item }) => (
+    <View style={{ flex: 1, flexDirection: 'row' }}>
+      <View
+        style={{
+          flex: 1,
+          height: 70,
+          justifyContent: 'center',
+          backgroundColor: '#fff',
+          margin: 5,
+          padding: 10,
+        }}
+      >
+        <Text style={{ color: '#333', fontSize: 16 }}>
+          {item.time} - {item.title}
+        </Text>
+        <Text style={{ color: '#AAA', fontSize: 12 }}>{item.feeling}</Text>
+      </View>
+      <View style={{ justifyContent: 'center' }}>
+        <Icon name="chevron-right" color="#999" size={22} />
+      </View>
+    </View>
+  );
+
+  render() {
+    const { meals } = this.state;
+    return (
+      <View style={styles.container}>
+        <View>
+          <StatusBar backgroundColor="#D6E7E0" />
+          <Text style={styles.dateLabel}>Segunda-feira, 23 de Abril de 2019</Text>
+        </View>
+        <View style={styles.content}>
+          <Text style={styles.contentTitle}>Hoje você comeu:</Text>
+          <FlatList data={meals} keyExtractor={this.keyExtractor} renderItem={this.renderItem} />
+        </View>
+      </View>
+    );
+  }
+}
 
 export default Main;
